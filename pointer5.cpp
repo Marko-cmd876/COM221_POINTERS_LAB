51b303c42d26c987997893ab1b8685ade1ace1b8
Marko <markokaunda2@gmail.com>
2026-03-31 11:21:10 +0200

    pointer program 5

---
+++ pointer5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int *p = &x;
+    int **pp = &p;
+
+    cout << "Value of x: " << x << endl;
+    cout << "Value using p: " << *p << endl;
+    cout << "Value using pp: " << **pp << endl;
+
+    return 0;
+}
